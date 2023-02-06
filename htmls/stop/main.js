fa95873fafea518a3cb4611f0ec44fba17a90bc9
doncode-oficial <doncode001@gmail.com>
2023-02-06 17:50:11 -0500

    Adding sockets and starting tests and creating the screen to begin the game

---
+++ htmls/stop/main.js
@@ -0,0 +1,20 @@
+// const socket = io("ws://localhost:3000");
+
+// socket.on("hello from server", (...args) => {
+//     console.log(...args);
+// })
+
+// socket.emit("hello from client", "hello world");
+
+
+function enterInGame(name) {
+    // Desaparecer el cartel del stop
+
+    // Agregar un contenedor flotante
+    // donde una vez los jugadores
+    // han ingresado al juego,
+    // se le pueda dar click
+    // a iniciar para poder comenzar el juego
+    let divGameColumns = document.getElementById("game-columns");
+    divGameColumns
+}
